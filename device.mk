37cca4f0bce7d72d6ba794c65a50d08412d42097
kleidione Freitas <kleidione@gmail.com>
2025-11-14 20:42:15 -0300

    onyx: Move dax-default to device tree
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -136,6 +136,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.config-V12-ndk.vendor  \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor 
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
