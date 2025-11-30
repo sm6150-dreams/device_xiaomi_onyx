b6da40c16f125446d346396e8d740bee0327f686
theshaenix <theshaenix@gmail.com>
2025-11-30 10:31:16 +0530

    onyx: Enable EPPE

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -8,6 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common mistOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Build
