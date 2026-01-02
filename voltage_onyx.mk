f4d5c81ba01d91c14e02fdbab6e7e836b2d3ecb9
sm6150-dreams <rn10prodev@gmail.com>
2026-01-02 19:41:16 +0100

    onyx: go voltageOS
    Signed-off-by: sm6150-dreams <rn10prodev@gmail.com>

--- voltage_onyx.mk
+++ voltage_onyx.mk
@@ -7,13 +7,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common voltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Build
+TARGET_BOOT_ANIMATION_RES := 2560
+TARGET_FACE_UNLOCK_SUPPORTED := true
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+EXTRA_UDFPS_ANIMATIONS := true
+TORCH_STR_SUPPORTED := true
 
 # Inherit from onyx device
 $(call inherit-product, device/xiaomi/onyx/device.mk)
 
-PRODUCT_NAME := lineage_onyx
+PRODUCT_NAME := voltage_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
