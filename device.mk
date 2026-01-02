db838d9afa814d240473480086cb1c7eae5ae1e6
sm6150-dreams <rn10prodev@gmail.com>
2026-01-02 19:42:34 +0100

    onyx: drop bcr call recorder
    Signed-off-by: sm6150-dreams <rn10prodev@gmail.com>

--- device.mk
+++ device.mk
@@ -116,9 +116,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-# Call the BCR setup
-$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
-
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
